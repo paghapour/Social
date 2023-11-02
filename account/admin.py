7feb1e21cefcd7bfc965d0d86c9677fad19d6036
paghapour <pouriaaghapour@gmail.com>
2023-11-02 20:19:05 +0330

    complete editing user

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Relation
+from .models import Relation, Profile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 
-@admin.register(Relation)
-class RelationAdmin(admin.ModelAdmin):
-    pass
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class ExtendUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendUserAdmin)
+admin.site.register(Relation)
--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from .models import Profile
 
 
 class UserRegistrationForm(forms.Form):
@@ -28,10 +29,18 @@ class UserRegistrationForm(forms.Form):
         p1 = cd.get('password1')
         p2 = cd.get('password2')
 
-        if p1 and p2 and p1 != p2 :
+        if p1 and p2 and p1 != p2:
             raise ValidationError('password must match')
 
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class EditUserForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = Profile
+        fields = ('age', 'bio')
